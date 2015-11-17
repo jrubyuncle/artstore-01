f0eef499c695f6d073fc3bd26152c67221595183
wesleychu <jrubyuncle@gmail.com>
2015-11-17 15:21:29 +0800

    wk2-4 use class_name: to specify model class name

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :user
 
-  has_one  :order_info
-  has_many :order_items
+  has_one  :info, class_name: "OrderInfo", dependent: :destroy
+  has_many :items, class_name: "OrderItem", dependent: :destroy
 end
