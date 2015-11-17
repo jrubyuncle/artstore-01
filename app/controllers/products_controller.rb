955bda5af19da6ff55f87af26ed76e0732654d0c
wesleychu <jrubyuncle@gmail.com>
2015-11-17 11:21:58 +0800

    wk2-2 add product to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,18 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  # /products/:id/add_to_cart(.:format)
+  def add_to_cart
+    @product = Product.find(params[:id])
+
+    if current_cart.items.include?(@product)
+      flash[:warning] = "你的購物車內已有此物品"
+    else
+      current_cart.add_product_to_me(@product)
+      flash[:notice] = "你已成功將 #{@product.title} 加入購物車"
+    end
+
+    redirect_to :back
+  end
 end
