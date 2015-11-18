7c239c0f79cbcfba8f2aaa6198b0e8990ec87999
wesleychu <jrubyuncle@gmail.com>
2015-11-18 12:23:40 +0800

    wk2-8 set state transition in order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ActiveRecord::Base
+  include AASM
+
   belongs_to :user
 
   has_one  :info, class_name: "OrderInfo", dependent: :destroy
@@ -34,4 +36,34 @@ class Order < ActiveRecord::Base
   def pay!
     self.update_columns(is_paid: true)
   end
+
+  aasm do
+    state :order_placed, initial: true
+    state :paid
+    state :shipping
+    state :shipped
+    state :order_cancelled
+    state :good_returned
+
+    event :make_payment do
+      transitions from: :order_placed, to: :paid
+    end
+
+    event :ship do
+      transitions from: :paid, to: :shipping
+    end
+
+    event :deliver do
+      transitions from: :shipping, to: :shipped
+    end
+
+    event :return_good do
+      trnasitions from: :shipped, to: :good_returned
+    end
+
+    event :cancel_order do
+      transitions from: [:order_placed, :paid], to: :order_cancelled
+  end
+
+
 end
